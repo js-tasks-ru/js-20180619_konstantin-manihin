92f5b5523c0dd2b6ed5d839eb4a249396615a55f
manikhin-km <manikhin-km@yandex.ru>
2018-06-30 14:20:44 +0500

    add task: 1-module/3-task

--- 1-module/3-task/index.js
+++ 1-module/3-task/index.js
@@ -7,5 +7,18 @@
  */
 function getMinMax(str) {
 
-}
+  let arr = str.split(' ').join().split(',');
+  let number = [];
+  let out = {};
 
+  arr.forEach(function(item, i, arr) {
+    if ( !isNaN(parseFloat(item)) ) {
+      number.push(parseFloat(item));
+    }
+  });
+
+  out.min = Math.min.apply(null, number);
+  out.max = Math.max.apply(null, number);
+
+  return out;
+}
