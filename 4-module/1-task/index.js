725e8ade6cdaafe8109d76401f4548a65be11251
manikhinkm <manikhin-km@yandex.ru>
2018-08-15 14:39:50 +0500

    add task: 4-module/1-task

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -6,4 +6,12 @@
  * @return {HTMLUListElement}
  */
 function makeFriendsList (friends) {
+
+    let ul = document.createElement('ul');
+
+    for (let i = 0; i < friends.length; i++) {
+        ul.appendChild(document.createElement('li')).innerHTML = `${friends[i].firstName} ${friends[i].lastName}`;
+    }
+
+    return ul;
 }
